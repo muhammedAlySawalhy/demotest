2e2927be31441f155721db76728808cd153b08fa
muhammedAlySawalhy <muh.aly@outlook.com>
2024-12-03 18:02:18 +0200

    finishing

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
   port: process.env.port || 3000,
   host: process.env.host || "localhost",
   mongouri:
-    "mongodb://muh_aly:M1u2h3a4@123@cluster0.ulf93yo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://muh_aly:M1u2h3a4@123@cluster0.ulf93yo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   mariauri:
     process.env.mariauri || "mysql://root:password@localhost:3306/mydatabase",
   secret: process.env.secret || "StrongSecretKey",
